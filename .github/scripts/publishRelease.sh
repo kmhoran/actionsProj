99594d73d01c850062760c8ab782573badb32311
kmhoran <kevin.michael.horan@gmail.com>
2021-02-24 11:06:37 -0800

    remove comments from publishRelease.sh

--- .github/scripts/publishRelease.sh
+++ .github/scripts/publishRelease.sh
@@ -41,9 +41,3 @@ git checkout -b ${RELEASE}
 
 echo "git push origin ${RELEASE}"
 git push origin ${RELEASE}
-
-
-#git branch -m release archive/20210218
-#git push origin --delete release
-#git branch -m sync-20210218 release
-#git push origin :sync-20210218 release
