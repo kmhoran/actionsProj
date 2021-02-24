fd5daa1d16b4e2fc4edf6dff0a19f673d281900e
kmhoran <kevin.michael.horan@gmail.com>
2021-02-24 09:31:56 -0800

    update publishRelease script

--- .github/scripts/publishRelease.sh
+++ .github/scripts/publishRelease.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
 echo "PUBLISHING RELEASE"
+
+local RELEASE = 'release'
+
+git branch
+
+#git branch -m release archive/20210218
+#git push origin --delete release
+#git branch -m sync-20210218 release
+#git push origin :sync-20210218 release
